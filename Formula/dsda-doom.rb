50133a52e4b7d0b5b5fd4b36ab22c72ed4036025
Pedro Beirao <82064173+Pedro-Beirao@users.noreply.github.com>
2021-09-11 10:56:43 +0100

    Update dsda-doom.rb

--- Formula/dsda-doom.rb
+++ Formula/dsda-doom.rb
@@ -3,7 +3,7 @@ class DsdaDoom < Formula
   homepage "https://github.com/kraflab/dsda-doom"
   url "https://github.com/kraflab/dsda-doom/archive/refs/tags/v0.21.3.tar.gz"
   license "GPL-2.0-only"
-  sha256 "f729402852a84d6b8f9c745483798f1c3daca1a46e740b0841a983d1a756ccd6"
+  sha256 "77819c6b4045d281cfdce8c6d5ca558d7b3161121c20aea4cdacdcdd5804b0df"
   head "https://github.com/kraflab/dsda-doom"
 
   depends_on "autoconf" => :build
