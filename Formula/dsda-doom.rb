d777a8d2fab12bfd9c356dcb9556ebb074e819ff
Pedro Beirao <82064173+Pedro-Beirao@users.noreply.github.com>
2021-12-01 19:39:19 +0000

    Update dsda-doom.rb

--- Formula/dsda-doom.rb
+++ Formula/dsda-doom.rb
@@ -1,7 +1,7 @@
 class DsdaDoom < Formula
   desc "This is a fork of prboom+ with a focus on speedrunning"
   homepage "https://github.com/kraflab/dsda-doom"
-  url "https://github.com/kraflab/dsda-doom/archive/refs/tags/v0.21.3.tar.gz"
+  url "https://github.com/kraflab/dsda-doom/archive/refs/tags/v0.22.2.tar.gz"
   license "GPL-2.0-only"
   sha256 "77819c6b4045d281cfdce8c6d5ca558d7b3161121c20aea4cdacdcdd5804b0df"
   head "https://github.com/kraflab/dsda-doom"
@@ -18,7 +18,6 @@ class DsdaDoom < Formula
   depends_on "portmidi"
   depends_on "sdl2"
   depends_on "sdl2_mixer"
-  depends_on "sdl2_net"
 
   def install
     system "cmake", "./prboom2"
